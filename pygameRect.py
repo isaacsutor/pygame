2e08d987ccde71b84ec5cb143386e401c63e0dd5
isaacsutor <isaac.sutor@gmail.com>
2018-11-30 00:28:21 -0500

    rocket enemies working. movement of player working. only rectangles. need to add images

--- pygameRect.py
+++ pygameRect.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import random
 
 """
 Isaac Sutor
@@ -11,10 +12,44 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         # create surface
-        self.surf = pygame.Surface((75, 75))
+        self.surf = pygame.Surface((75, 25))
         # color surface
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[pygame.K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[pygame.K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[pygame.K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[pygame.K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        # keep player on screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > 800:
+            self.rect.left = 800
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        elif self.rect.bottom >= 600:
+            self.rect.bottom = 600
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.Surface((20, 10))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(center=(random.randint(820, 900), random.randint(0, 600)))
+        self.speed = random.randint(5, 20)
+
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
 
 
 # initialize
@@ -23,7 +58,18 @@ pygame.init()
 # create screen
 screen = pygame.display.set_mode((800, 600))
 
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 250)
+
 player = Player()
+
+
+background = pygame.Surface((screen.get_size()))
+background.fill((0, 0, 0))
+
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 # Variable to keep main loop running
 running = True
@@ -36,10 +82,21 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-            # check for QUIT
-            elif event.type == pygame.QUIT:
-                running = False
+        # check for QUIT
+        elif event.type == pygame.QUIT:
+            running = False
+        elif event.type == ADDENEMY:
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+    screen.blit(background, (0, 0))
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+    enemies.update()
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
 
-    # draw rect
-    screen.blit(player.surf, (400, 300))
+    if pygame.sprite.spritecollideany(player, enemies):
+        player.kill()
+
     pygame.display.flip()
